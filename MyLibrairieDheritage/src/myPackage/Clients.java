450412154f4237790d18f93165fc802fcd1d4b73
ThersiquelG <gregthersiquel@hotmail.fr>
2018-07-30 12:09:36 +0200

    création et génération de la classe "CLients" et des getters/setters

---
+++ MyLibrairieDheritage/src/myPackage/Clients.java
@@ -0,0 +1,18 @@
+package myPackage;
+
+public class Clients {
+	public String nom;
+	public String prénom;
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrénom() {
+		return prénom;
+	}
+	public void setPrénom(String prénom) {
+		this.prénom = prénom;
+	}
+}
