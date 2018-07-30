86c2a01a56b2b49a26d7aac6a9b23a0f2ccbaa89
ThersiquelG <gregthersiquel@hotmail.fr>
2018-07-30 12:08:20 +0200

    Création de la classe BD et génération des getters/setters

---
+++ MyLibrairieDheritage/src/myPackage/BD.java
@@ -0,0 +1,14 @@
+package myPackage;
+
+public class BD extends Produits {
+public String dessinateur;
+
+public String getDessinateur() {
+	return dessinateur;
+}
+
+public void setDessinateur(String dessinateur) {
+	this.dessinateur = dessinateur;
+}
+
+}
