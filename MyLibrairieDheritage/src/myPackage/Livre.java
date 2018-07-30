06280ac51ba8a5dae1fd0ae7b053f0784ad5d993
ThersiquelG <gregthersiquel@hotmail.fr>
2018-07-30 12:06:40 +0200

    Classe "livre" terminée et getters/setters générés

--- MyLibrairieDheritage/src/myPackage/Livre.java
+++ MyLibrairieDheritage/src/myPackage/Livre.java
@@ -1,5 +1,14 @@
 package myPackage;
 
 public class Livre extends Produits{
+public String type;
+
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
 
 }
