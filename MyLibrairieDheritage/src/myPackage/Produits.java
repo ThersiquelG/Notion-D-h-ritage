d1e309892d4bfb1265a95ef4933224d5061b629e
ThersiquelG <gregthersiquel@hotmail.fr>
2018-07-30 12:03:05 +0200

    Classe produits terminée et getters/setters générés

--- MyLibrairieDheritage/src/myPackage/Produits.java
+++ MyLibrairieDheritage/src/myPackage/Produits.java
@@ -1,5 +1,40 @@
 package myPackage;
 
 public class Produits {
+	public String Titre;
+	public String Auteur;
+	public String Editeur;
+	public boolean disponibilite;
+	public double prix;
+	public String getTitre() {
+		return Titre;
+	}
+	public void setTitre(String titre) {
+		Titre = titre;
+	}
+	public String getAuteur() {
+		return Auteur;
+	}
+	public void setAuteur(String auteur) {
+		Auteur = auteur;
+	}
+	public String getEditeur() {
+		return Editeur;
+	}
+	public void setEditeur(String editeur) {
+		Editeur = editeur;
+	}
+	public boolean isDisponibilite() {
+		return disponibilite;
+	}
+	public void setDisponibilite(boolean disponibilite) {
+		this.disponibilite = disponibilite;
+	}
+	public double getPrix() {
+		return prix;
+	}
+	public void setPrix(double prix) {
+		this.prix = prix;
+	}
 
 }
